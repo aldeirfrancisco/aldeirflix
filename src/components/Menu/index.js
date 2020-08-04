e0821d17eb1dd1f0671ffa774deb3d2a3e333d6c
Aldeir Francisco <dide123francisco@gmail.com>
2020-08-03 23:24:36 -0300

    pages, categoria , home

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Logo from '../../assets/img/logo.png';
 import  './Menu.css';
+import {Link} from'react-router-dom';
 //import ButtonLink from './components/ButtonLink';
 import Button from '../Button';
 
 function Menu(){
     return (
      <div className="Menu">
-         <a href="/">
+         <Link to="/">
             <img className="Logo" src={Logo} alt="Aldeirflix Logo" />   
-         </a>
-         <Button as="a" className="ButtonLink" href="/">
+         </Link>
+         <Button as={Link} className="ButtonLink" to="/cadastro/video">
              Novo vídeo
          </Button>
      </div>
